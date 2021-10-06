ffb1fe4f15974ceb012f06c9344c1df47d833407
will-cesar <willcesar010421@gmail.com>
2021-10-05 22:51:36 -0300

    Aula 26 - uso do custom hook

--- README.md
+++ README.md
@@ -4,7 +4,17 @@
 > Link da playlist: https://youtube.com/playlist?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d
 
 ## Conteúdo do curso
+- Construção de um blog com os recursos básicos do React
+- Uso dos principais hooks como useState, useEffect, useParams
+- Tratamento de erros de requisição
+- Uso do json-server como fake api
+- Criação de um custom hook (no projeto foi criado um "useFetch" hook)
+- Utilização do react-router-dom. Criação de rotas normais e rotas com parâmetros
+- Uso do "AbortController" para cancelar o useEffect
 
 ## Dependências
+- json-server
 
 ## Run App
+- npm start -> comando para rodar o app em ambiente de desenvolvimento
+- npm run backend -> comando para rodar o fake backend com o json-server
--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,14 +1,32 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import BlogDetails from "./BlogDetails";
+import Create from "./Create";
 import Home from "./Home";
 import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
